dff1e1c4f50c463c748fd178e3ab8ef73103aabd
zacktmiller92 <zacktmiller92@gmail.com>
2021-11-28 16:01:28 -0500

    added boilerplate for navigation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import './App.css';
+import Home from './pages/Home'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Home />
 
       </header>
     </div>
---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navigation from '../components/Navigation'
+import { Container } from 'react-bootstrap';
+
+
+
+function Home() {
+
+    return (
+
+        <Navigation />
+
+    );
+};
+
+export default Home;
